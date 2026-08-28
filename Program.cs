1fbd29b3b7b8b734e91fe22e88f35c56994e80da
RodrigoGarciaguirre <rodrigarciaguirre@gmail.com>
2026-08-28 04:58:43 -0600

    Variables y tipos de datos

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TorettoRacing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// C# Corredor
+            string  nombre      = "Brian O'Conner";
+            int     edad        = 25;
+            double  velocidad   = 310.5;
+            decimal combustible = 60.5m;
+            bool    aprobado    = true;
+            char    categoria   = 'A';
+		}
+	}
+}
+
